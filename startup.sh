045db66a9bf49c91742c28a43709d1054e8534fd
Nerya Hadad <neryahadad@gmail.com>
2025-04-10 01:00:11 +0200

    🚀 Switch to gunicorn in startup.sh for Azure

--- startup.sh
+++ startup.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-export FLASK_APP=api_server.py
-export FLASK_RUN_HOST=0.0.0.0
-export FLASK_RUN_PORT=8000
 pip install -r requirements.txt
-flask run
+exec gunicorn -w 4 -b 0.0.0.0:$PORT api_server:app
